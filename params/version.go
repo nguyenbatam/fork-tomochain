2aa7d17b4e37949dc64b034f15321de78370e3dd
Nguyen Ba Tam <nguyenbatam90@gmail.com>
2019-11-29 11:39:28 +0700

    setup genesis & network id default

--- params/version.go
+++ params/version.go
@@ -23,8 +23,8 @@ import (
 const (
 	VersionMajor = 1        // Major version component of the current release
 	VersionMinor = 5        // Minor version component of the current release
-	VersionPatch = 4        // Patch version component of the current release
-	VersionMeta  = "stable" // Version metadata to append to the version string
+	VersionPatch = 5        // Patch version component of the current release
+	VersionMeta  = "chainos" // Version metadata to append to the version string
 )
 
 // Version holds the textual version string.
